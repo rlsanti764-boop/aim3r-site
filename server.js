a9a3503fde76621a0f284384eefa136cc25a104c
rlsanti764-boop <rlsanti764@gmail.com>
2026-04-15 20:19:08 +0100

    Update server.js

--- server.js
+++ server.js
@@ -1,29 +1,40 @@
 import express from "express";
 import cors from "cors";
 import OpenAI from "openai";
+import fetch from "node-fetch";
 
 const app = express();
 app.use(cors());
 app.use(express.json());
 
-const client = new OpenAI({
+const openai = new OpenAI({
   apiKey: process.env.OPENAI_API_KEY,
 });
 
+
+// 🤖 CHAT IA
 app.post("/chat", async (req, res) => {
-  try {
-    const userMessage = req.body.message;
+  const response = await openai.responses.create({
+    model: "gpt-4.1-mini",
+    input: req.body.message,
+  });
 
-    const response = await client.responses.create({
-      model: "gpt-4.1-mini",
-      input: userMessage,
-    });
-
-    res.json({ reply: response.output_text });
-  } catch (err) {
-    console.error(err);
-    res.status(500).json({ error: "Erro no servidor" });
-  }
+  res.json({ reply: response.output_text });
 });
 
-app.listen(3000, () => console.log("Server running on port 3000"));
+
+// 🎞️ GIF API (tenor)
+app.get("/gif", async (req, res) => {
+  const q = req.query.q;
+
+  const url = `https://tenor.googleapis.com/v2/search?q=${q}&key=LIVDSRZULELA&limit=1`;
+
+  const response = await fetch(url);
+  const data = await response.json();
+
+  res.json({
+    url: data.results[0].media_formats.gif.url
+  });
+});
+
+app.listen(3000, () => console.log("Server ON"));
